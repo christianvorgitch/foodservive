3bb05c807b9a563a04b18daadc8c171c2f2f2a25
christianvorgitch <47836848+christianvorgitch@users.noreply.github.com>
2019-08-21 18:14:44 -0500

    UPDATE

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,4 +1,16 @@
-$(document).ready(function () {
+$(document).ready(function() {
+    
+    $('.js--section-features').waypoint(function(direction) {
+        if (direction == "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+        
+    }, {
+        offset: '60px;'
+    });
+    
     
     /*
     var waypoints = $('#handler-first').waypoint(function(direction) {
