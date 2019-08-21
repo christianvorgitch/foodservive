2e52ac3299c4637e31b54acf1fcd058e53b35475
christianvorgitch <47836848+christianvorgitch@users.noreply.github.com>
2019-08-21 17:11:55 -0500

    update

---
+++ resources/js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    
+    /*
+    var waypoints = $('#handler-first').waypoint(function(direction) {
+  notify(this.element.id + ' hit 25% from top of window') 
+}, {
+  offset: '25%'
+})
+*/
+    
+    
+});
